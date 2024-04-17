2bda2ca9227af5e1e07be0961355ddbffb4a78a1
stix2you <stix2you@gmail.com>
2024-04-17 14:01:08 -0500

    Updated Screen2.js

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,23 +1,47 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 const Screen1 = ({ navigation }) => {
- return (
-   <View style={styles.container}>
-     <Text>Hello Screen1!</Text>
-     <Button
-       title="Go to Screen 2"
-       onPress={() => navigation.navigate('Screen2')}
-     />
-   </View>
- );
+   const [name, setName] = useState('');
+
+   return (
+      <View style={styles.container}>
+         <Text style={styles.textDisplay}>Hello Screen1!</Text>
+         <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={setName}
+            placeholder='Type your username here'
+         />
+         <Button
+            title="Go to Screen 2"
+            onPress={() => navigation.navigate('Screen2', { name: name})}
+         />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
- }
+   container: {
+      flex: 1,
+      backgroundColor: '#00ffff',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   textInput: {
+      width: "88%",
+      fontSize: 20,
+      padding: 15,
+      borderColor: 'black',
+      borderWidth: 1,
+      borderRadius: 10,
+      marginTop: 15,
+      marginBottom: 15
+   },
+   textDisplay: {
+      color: 'darkslateblue',
+      fontSize: 20,
+   },
 });
 
 export default Screen1;
--- components/Screen2.js
+++ components/Screen2.js
@@ -1,19 +1,26 @@
+import { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-const Screen2 = () => {
- return (
-   <View style={styles.container}>
-     <Text>Hello Screen2!</Text>
-   </View>
- );
+const Screen2 = ({ route, navigation }) => {
+   const { name } = route.params;
+
+   useEffect(() => {
+      navigation.setOptions({ title: name });
+   }, []);
+
+   return (
+      <View style={styles.container}>
+         <Text>Hello Screen2!</Text>
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
- }
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+   }
 });
 
 export default Screen2;
